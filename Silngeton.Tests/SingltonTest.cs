6b2ae0d80715ab7721b2901bb98fc9150169fb64
RuslanPr0g <theruslan.prog@gmail.com>
2021-01-20 15:59:05 +0300

    change references

---
+++ Silngeton.Tests/SingltonTest.cs
@@ -0,0 +1,21 @@
+﻿using Singleton.Servers;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace Silngeton.Tests
+{
+    public class SingltonTest
+    {
+        static readonly TableServers hosts = TableServers.GetTableServers();
+        static readonly TableServers premiumHosts = TableServers.GetTableServers();
+
+        [Theory]
+        [InlineData]
+        public void ShouldNotReturnTheSameInstance()
+        {
+
+        }
+    }
+}
